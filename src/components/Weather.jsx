852b25392c657fc4a68ff30568ee28a269b07dbb
Arash Harandehi <56218691+ItsMeArash@users.noreply.github.com>
2023-06-21 16:29:45 +0330

    loading spinner added to weather

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -2,10 +2,13 @@ import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { fetchWeatherGPS } from "../helpers/weatherAPI";
 import { useSelector } from "react-redux";
+import { BounceLoader } from "react-spinners";
+
 
 function Weather() {
   const [showMenu, setShowMenu] = useState(false);
   const [weather, setWeather] = useState(null);
+  const [isLoading, setIsLoading] = useState(false); // add a loading state variable
 
   const isLoggedIn = useSelector((state) => state.login.isLoggedIn);
   const navigate = useNavigate();
@@ -19,6 +22,26 @@ function Weather() {
   useEffect(() => {
     console.log(weather);
   }, [weather]);
+
+  const handleFetchWeather = () => {
+    setIsLoading(true); // set the loading state to true when fetching weather data
+    fetchWeatherGPS()
+      .then((weather) => {
+        setWeather(weather);
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => {
+        setIsLoading(false); // set the loading state to false when the data has been fetched
+      });
+  };
+
+  const override = {
+    display: "block",
+    margin: "0 auto",
+    borderColor: "red",
+  };
 
   return (
     <div>
@@ -80,62 +103,61 @@ function Weather() {
           </ul>
         </div>
         <div className="flex-1 flex flex-col items-center justify-center">
-  <div className="flex justify-center items-center">
-    <input
-      className="py-2 px-3 bg-gray-200 rounded-l-lg w-64"
-      type="text"
-      placeholder="Enter city name here"
-    />
-    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r-lg focus:outline-none focus:shadow-outline">
-      Search
-    </button>
+          <div className="flex justify-center items-center">
+            <input
+              className="py-2 px-3 bg-gray-200 rounded-l-lg w-64"
+              type="text"
+              placeholder="Enter city name here"
+            />
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r-lg focus:outline-none focus:shadow-outline">
+              Search
+            </button>
 
-    <button
-      onClick={() => {
-        fetchWeatherGPS()
-          .then((weather) => {
-            setWeather(weather);
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-      }}
-      className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth="2.0"
-        stroke="currentColor"
-        className="w-6 h-6"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
-        />
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
-        />
-      </svg>
-    </button>
-  </div>
-  {weather && (
-    <div className="mt-16 rounded-lg shadow-md bg-white p-4 flex flex-col items-center">
-      <img
-        className="w-20 h-20 mb-4"
-        src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
-      />
-      <h3 className="text-2xl font-bold mb-2">{`${Math.floor(weather.main.temp)}°C`}</h3>
-      <p className="text-gray-500 mb-4">{weather.weather[0].description}</p>
-      <h4 className="text-lg font-bold mb-2">{`${weather.name}, ${weather.sys.country}`}</h4>
-      <p className="text-gray-500">{`Lat: ${weather.coord.lat} | Lon: ${weather.coord.lon}`}</p>
-    </div>
-  )}
-</div>
+            <button
+              onClick={handleFetchWeather}
+              className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="2.0"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+                />
+              </svg>
+            </button>
+          </div>
+          {isLoading && (
+           <BounceLoader className="mt-16" css={override} color={"#123abc"} loading={isLoading} />
+          )}
+          {weather && !isLoading && (
+            <div className="mt-16 rounded-lg shadow-md bg-white p-4 flex flex-col items-center">
+              <img
+                className="w-20 h-20 mb-4"
+                src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+              />
+              <h3 className="text-2xl font-bold mb-2">{`${Math.floor(
+                weather.main.temp
+              )}°C`}</h3>
+              <p className="text-gray-500 mb-4">
+                {weather.weather[0].description}
+              </p>
+              <h4 className="text-lg font-bold mb-2">{`${weather.name}, ${weather.sys.country}`}</h4>
+              <p className="text-gray-500">{`Lat: ${weather.coord.lat} | Lon: ${weather.coord.lon}`}</p>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
